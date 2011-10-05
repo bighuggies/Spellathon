dcf53c9337550507a12f146f402844f4c92292a1
Andrew Hughson <kungfu.spike@gmail.com>
2011-10-05 21:41:04 +1300

    Spellathon driver

--- spellathon.py
+++ spellathon.py
@@ -3,23 +3,11 @@ Created on Sep 29, 2011
 
 @author: ahug048
 '''
-from aid.views import *
-from aid.widgets import *
-from Tkinter import *
+from aid.views import Logon
+from Tkinter import Tk
 
 if __name__ == "__main__":
     root = Tk()
-    
-        
+    root.wm_withdraw()
     logon = Logon(root)
-    logon.grid(column=1, row=1)
-    admin = Administration(root)
-    wlcm = WelcomeScreen(root)
-    score = Score(root, btncolumn=1)
-    aid = SpellingAid(root)
-    spc = SpellingComplete(root, btncolumn=0)
-    reg = UserRegistration(root, btncolumn=0)
-    auth = Authoriser(root, btncolumn=0)
-    adm = CreateAdmin(root, btncolumn=0)
-
     root.mainloop()
